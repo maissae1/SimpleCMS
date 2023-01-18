2c08fcef57d34427aa2f10dd9289acfab8543c3b
maissae amzghar <104233004+maissae1@users.noreply.github.com>
2023-01-18 21:26:05 +0000

    add names to urls

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from posts import views
 
 urlpatterns = [
-    path('', views.index),
-    path('login/', views.login_view)
+    path('', views.index, name='home'),
+    path('login/', views.login_view, name='login')
 ]
