8ed363b3aeace1b794eda98f20df08268268dc43
maissae amzghar <104233004+maissae1@users.noreply.github.com>
2023-01-20 14:24:43 +0000

    add Post model with migrations

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    publish_date = models.DateTimeField()
+    created_date = models.DateTimeField(auto_now_add=True)
