13b0a4b1ebad131320cb3de08821700c2433966f
maissae amzghar <104233004+maissae1@users.noreply.github.com>
2023-01-20 14:31:36 +0000

    add post urls

--- posts/urls.py
+++ posts/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('accounts', views.accounts, name='accounts'),
     path('accounts/new', views.create_account, name='create-account'),
     path('accounts/<int:id>/update', views.update_account, name='update-account'),
-    path('accounts/<int:id>/delete', views.delete_account, name='delete-account')
+    path('accounts/<int:id>/delete', views.delete_account, name='delete-account'),
+    path('posts/', views.posts, name='posts'),
+    path('posts/new', views.create_post, name='create-post'),
+    path('posts/<int:id>/update', views.update_post, name='update-post'),
+    path('posts/<int:id>/delete', views.delete_post, name='delete-post')
 ]
